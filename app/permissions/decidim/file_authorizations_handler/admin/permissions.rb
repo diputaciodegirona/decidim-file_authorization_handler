01924d357ee2a9d052010ae66ac4f7451e0b63e1
Oliver Valls <oliver.vh@coditramuntana.com>
2018-12-28 15:53:46 +0100

    [FIX] Mistyped method name.

--- app/permissions/decidim/file_authorizations_handler/admin/permissions.rb
+++ app/permissions/decidim/file_authorizations_handler/admin/permissions.rb
@@ -9,7 +9,7 @@ module Decidim
           return permission_action if permission_action.scope != :admin
           return Decidim::Proposals::Admin::Permissions.new(user, permission_action, context).permissions if permission_action.scope == :admin
           if user.organization.available_authorizations.include?("file_authorization_handler")
-            if permission_action_in(:manage, :read)
+            if permission_action_in?(:manage, :read)
               allow! if permission_action.subject == Decidim::FileAuthorizationHandler::CensusDatum
             end
           end
